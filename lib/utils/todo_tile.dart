131d6811d71eb42fdb4a2169b919a3705d760c6c
Saydullo <abdurashidovsaydullo35@gmail.com>
2024-05-07 01:48:10 +0500

    ToDoTile and Checkbox is completed.. 01:28:50

--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -14,17 +14,17 @@ class ToDoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25, top: 25),
       child: Container(
         padding: EdgeInsets.all(24),
-        child: Row(
-          children: [
-            Checkbox(value: taskCompleted, onChanged: onChanged),
-            Text(taskName),
-          ],
-        ),
         decoration: BoxDecoration(
             color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          children: [
+            Checkbox(value: taskCompleted, onChanged: onChanged, activeColor: Colors.black,),
+            Text(taskName,style: TextStyle(decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none),),
+          ],
+        ),
       ),
     );
   }
